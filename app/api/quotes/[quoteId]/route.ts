8fbb42e33b9c04c378d6621496601b7d9de2ff66
Juan Vianney NM <juanvianneynm@gmail.com>
2024-11-28 00:22:04 +0100

    fix bug

--- app/api/quotes/[quoteId]/route.ts
+++ app/api/quotes/[quoteId]/route.ts
@@ -1,15 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
 import * as quoteService from "@/services/quote.service";
 
-export async function GET(request: Request, { params }: { params: { quoteId: string } }) {
-  try {
-    const {quoteId} = params;
-    const quote = await quoteService.getQuoteById(quoteId);
-    return NextResponse.json({ quote }, { status: 200 });
-  } catch (e) {
-    return NextResponse.json({ error: (e as Error).message }, { status: 500 });
+export async function GET(req: NextRequest, { params }: { params: { quoteId: string } }) {
+    try {
+      const { quoteId } = params;
+  
+      const quote = await quoteService.getQuoteById(quoteId);
+      if (!quote) {
+        return NextResponse.json({ error: "Quote not found" }, { status: 404 });
+      }
+      return NextResponse.json({ quote }, { status: 200 });
+    } catch (e) {
+      console.error("Error fetching quote:", e);
+      return NextResponse.json({ error: (e as Error).message }, { status: 500 });
+    }
   }
-}
 
 export async function PUT(req: NextRequest, {params}: {params: {quoteId: string}}) {
   try {
