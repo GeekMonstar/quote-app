650cb771e578eb4066edf13ec99c18294948d16e
Juan Vianney NM <juanvianneynm@gmail.com>
2024-11-29 16:54:03 +0100

    bug fix

--- app/api/quotes/[quoteId]/route.ts
+++ app/api/quotes/[quoteId]/route.ts
@@ -1,30 +1,19 @@
-import { NextRequest, NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 import * as quoteService from "@/services/quote.service";
 
-export async function GET(
-    req: Request,
-    context: { params: { quoteId: string } }
-  ) {
-    try {
-      const { quoteId } = context.params;
-      const quote = await quoteService.getQuoteById(quoteId);
-  
-      if (!quote) {
-        return NextResponse.json({ error: "Quote not found" }, { status: 404 });
-      }
-  
-      return NextResponse.json({ quote });
-    } catch (error) {
-      return NextResponse.json(
-        { error: (error as Error).message },
-        { status: 500 }
-      );
-    }
-  }
-
-export async function PUT(req: NextRequest, {params}: {params: {quoteId: string}}) {
+export async function GET(req: NextRequest, {params}: {params: Promise<{quoteId: string}>}) {
   try {
-    const {quoteId} = params;
+    const {quoteId} = await params;
+    const quote = await quoteService.getQuoteById(quoteId);
+    return NextResponse.json({ quote }, { status: 200 });
+  } catch (e) {
+    return NextResponse.json({ error: (e as Error).message }, { status: 500 });
+  }
+}
+
+export async function PUT(req: NextRequest, {params}: {params: Promise<{quoteId: string}>}) {
+  try {
+    const {quoteId} = await params;
     const body = await req.json();
     const { quote } = body;
     let originalQuote = await quoteService.getQuoteById(quoteId);
@@ -36,9 +25,9 @@ export async function PUT(req: NextRequest, {params}: {params: {quoteId: string}
   }
 }
 
-export async function DELETE(req: NextRequest, {params}: {params: {quoteId: string}}) {
+export async function DELETE(req: NextRequest, {params}: {params: Promise<{quoteId: string}>}) {
   try {
-    const {quoteId} = params;
+    const {quoteId} = await params;
     const quote = await quoteService.getQuoteById(quoteId);
     await quoteService.deleteQuote(quoteId);
     return NextResponse.json({ quote }, { status: 200 });
--- app/api/quotes/authors/[author]/route.ts
+++ app/api/quotes/authors/[author]/route.ts
@@ -1,9 +1,9 @@
 import * as quoteService from '@/services/quote.service';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(req: NextRequest, {params}: {params: {author: string}}){
+export async function GET(req: NextRequest, {params}: {params: Promise<{author: string}>}){
     try{
-        const {author} = params;
+        const {author} = await params;
         const quotes = await quoteService.getQuotesByAuthor(author);
         return NextResponse.json({quotes});
     }catch(e){
