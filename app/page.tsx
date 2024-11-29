32e8318441f69cf10acb56dd02f4657c877171ad
Juan Vianney NM <juanvianneynm@gmail.com>
2024-11-29 17:03:51 +0100

    make ssr by default

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import QuoteStack from "@/components/QuoteStack";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-100">
